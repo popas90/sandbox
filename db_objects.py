827da0c9353cb73fb9f8736b1a752be38a120aeb
popas <bogdititupopa@gmail.com>
2015-10-26 18:41:36 +0200

    added Chunk class

--- db_objects.py
+++ db_objects.py
@@ -10,19 +10,26 @@ class File(Base):
     __tablename__ = 'file'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    parent_id = Column(Integer, ForeignKey('folder.id'))
-    # Using SHA256 for the checksum
     checksum = Column(String(64), nullable=False)
+    folder_id = Column(Integer, ForeignKey('folder.id'))
+    chunks = relationship("Chunk")
+
+
+class Chunk(Base):
+    __tablename__ = 'chunk'
+    id = Column(Integer, primary_key=True)
+    checksum = Column(String(64), nullable=False)
+    file_id = Column(Integer, ForeignKey('file.id'), nullable=False)
+    index_in_file = Column(Integer, nullable=False)
 
 
 class Folder(Base):
     __tablename__ = 'folder'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    files = relationship("File")
-    parent_id = Column(Integer, ForeignKey('folder.id'))
-    # Using SHA256 for the checksum
     checksum = Column(String(64), nullable=False)
+    parent_folder_id = Column(Integer, ForeignKey('folder.id'))
+    files = relationship("File")
 
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file.
